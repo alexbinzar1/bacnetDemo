a19b56d77f3330a70886c4eb04c2477d9d86948b
Alexandru Binzar <alex.binzar@hibyte.ro>
2022-05-10 12:18:50 +0300

    added new test

--- src/main/java/de/breuer/bacnetdemo/service/BacnetService.java
+++ src/main/java/de/breuer/bacnetdemo/service/BacnetService.java
@@ -21,9 +21,7 @@ public class BacnetService {
 
     public static LocalDevice setUp() throws Exception {
         IpNetwork network = new IpNetworkBuilder().withBroadcast("192.168.0.138", 1883).build();
-
         Transport transport = new DefaultTransport(network);
-        System.out.println("inside main...." + transport);
         transport.setTimeout(500);
         transport.setSegTimeout(150);
         return new LocalDevice(1, transport);
@@ -37,20 +35,6 @@ public class BacnetService {
     }
 
     public static void send(LocalDevice d, UnconfirmedRequestService s) throws Exception {
-//        String macAddress = "a4:83:e7:51:0f:ae"
-//        String[] macAddressParts = macAddress.split(":");
-//
-//        // convert hex string to byte values
-//        byte[] macAddressBytes = new byte[6];
-//        for(int i=0; i<6; i++){
-//            Integer hex = Integer.parseInt(macAddressParts[i], 16);
-//            macAddressBytes[i] = hex.byteValue();
-//        }
-//        RemoteDevice remoteDevice = d.getRemoteDevice(1).get();
           d.send(d.getLocalBroadcastAddress(),s);
-//        return d.send(new Address(macAddressBytes),s).get();
-//        Address a = new Address(InetAddrCache.get("localhost", 1956));
-//        return d.send(a, null, MaxApduLength.UP_TO_50, Segmentation.segmentedBoth, s);
-//        return null; //TODO implement
     }
 }
--- src/test/java/de/breuer/bacnetdemo/BacnetDemoApplicationTests.java
+++ src/test/java/de/breuer/bacnetdemo/BacnetDemoApplicationTests.java
@@ -1,39 +1,61 @@
 package de.breuer.bacnetdemo;
 
-import com.serotonin.bacnet4j.npdu.ip.IpNetworkUtils;
-import com.serotonin.bacnet4j.service.confirmed.CreateObjectRequest;
+import com.serotonin.bacnet4j.LocalDevice;
 import com.serotonin.bacnet4j.service.unconfirmed.UnconfirmedCovNotificationRequest;
 import com.serotonin.bacnet4j.type.constructed.SequenceOf;
 import com.serotonin.bacnet4j.type.enumerated.ObjectType;
 import com.serotonin.bacnet4j.type.primitive.ObjectIdentifier;
 import com.serotonin.bacnet4j.type.primitive.UnsignedInteger;
-import com.serotonin.bacnet4j.util.sero.IpAddressUtils;
 import de.breuer.bacnetdemo.service.BacnetService;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.stream.IntStream;
 
 @SpringBootTest
 class BacnetDemoApplicationTests {
 
+    private LocalDevice ld;
 
+    private int SUB_IDENT = 18;
 
-    @Test
-    void testSetup() throws Exception {
-//        bacnetService.setUp();
-        IpNetworkUtils.getLocalInterfaceAddresses();
+    private int DEVICE = 100;
+
+    private int ANALOG_INPUT = 10;
+
+    private int TIME_REMAINING = 0;
+
+    private long SLEEP_TIME = 50;
+
+    @BeforeAll
+    void setUp() throws Exception {
+        ld = BacnetService.setUp();
+        ld.initialize();
     }
 
     @Test
-    void contextLoads() throws Exception {
-        var ld = BacnetService.setUp();
-        ld.initialize();
-         BacnetService.send(ld,
-                new UnconfirmedCovNotificationRequest(new UnsignedInteger(18), new ObjectIdentifier(ObjectType.device, 4), new ObjectIdentifier(
-                        ObjectType.analogInput, 10), new UnsignedInteger(0), new SequenceOf<>(BacnetService.generateValues()))
+    void testSimpleMessage() throws Exception {
+        BacnetService.send(ld,
+                new UnconfirmedCovNotificationRequest(new UnsignedInteger(SUB_IDENT), new ObjectIdentifier(ObjectType.device, DEVICE), new ObjectIdentifier(
+                        ObjectType.analogInput, ANALOG_INPUT), new UnsignedInteger(0), new SequenceOf<>(BacnetService.generateValues()))
         );
-        //ObjectType.analogInput, )
-//        System.out.println("some result: " + result);
+    }
+
+    @Test
+    void test100Req() {
+        IntStream.range(0,100)
+                .forEach(i-> {
+                    try {
+                        BacnetService.send(ld,
+                                new UnconfirmedCovNotificationRequest(new UnsignedInteger(SUB_IDENT), new ObjectIdentifier(ObjectType.device, DEVICE), new ObjectIdentifier(
+                                        ObjectType.analogInput, ANALOG_INPUT), new UnsignedInteger(TIME_REMAINING), new SequenceOf<>(BacnetService.generateValues()))
+                        );
+                        Thread.sleep(SLEEP_TIME);
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                });
     }
 
 }
